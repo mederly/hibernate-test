69689cd7c925ef1c5e486b4bb8d95bbd852aa07e
Pavol Mederly <mederly@evolveum.com>
2018-02-02 14:00:33 +0100

    Add forgotten plain case

---
+++ src/main/java/simple/Child.java
@@ -0,0 +1,33 @@
+package simple;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Child implements Serializable {
+
+	private Parent parent;
+	private String value;
+
+	public Child() {
+	}
+
+	@Id
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	public Parent getParent() {
+		return parent;
+	}
+
+	public void setParent(Parent parent) {
+		this.parent = parent;
+	}
+
+	@Id
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
---
+++ src/main/java/simple/TestMergeParentChild.java
@@ -0,0 +1,53 @@
+package simple;
+
+import org.hibernate.Session;
+
+public class TestMergeParentChild {
+    public static void main(String[] args) {
+        try {
+            create();
+            System.out.println("**********************************************************************************");
+            update();
+        } finally {
+            HibernateUtil.shutdown();
+        }
+    }
+
+    private static void create() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Parent parent = new Parent();
+        parent.setId(10);
+
+        Child child = new Child();
+        child.setParent(parent);
+        parent.getChildren().add(child);
+        child.setValue("old");
+
+        session.save(parent);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    private static void update() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Parent parent = new Parent();
+        parent.setId(10);
+
+        Child child = new Child();
+        child.setParent(parent);
+        parent.getChildren().add(child);
+        child.setValue("new");
+
+        session.merge(parent);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
